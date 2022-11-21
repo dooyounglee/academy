33a655b5ab3e878480108d280df1512ced8c170b
doo <gostbaducking1@nate.com>
2022-11-22 02:05:24 +0900

    feat: dypc001controller test

--- src/test/java/com/doo/academy/dy/pc/web/DyPc001ControllerTest.java
+++ src/test/java/com/doo/academy/dy/pc/web/DyPc001ControllerTest.java
@@ -16,15 +16,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +49,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import com.doo.academy.dy.model.pk.ContestPK;
 import com.doo.academy.dy.model.ps.Contest;
 import com.doo.academy.dy.model.us.User;
 import com.doo.academy.dy.pc.service.DyPc001Service;
@@ -61,6 +65,7 @@ import com.doo.academy.dy.us.service.DyUs001Service;
 // @AutoConfigureMockMvc
 // @ActiveProfiles(value = "test")
 @TestInstance(Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DyPc001ControllerTest {
 
 	@Autowired private MockMvc mockMvc;
@@ -72,19 +77,15 @@ public class DyPc001ControllerTest {
 	private DyUs001Repository dyUs001Repository;
 	
 	@Test
-	@DisplayName(value = "save")
-	//@WithUserDetails(value = "admin@doo.com")
+	@DisplayName(value = "findAll")
+	// @WithUserDetails(value = "admin@doo.com")
 	@WithMockUser(username = "admin@doo.com", password = "admin", roles = {"ADMIN"})
-	@WithAnonymousUser
+	// @WithAnonymousUser
 	@Order(1)
-	public void saveTest() throws Exception {
+	public void findAllTest() throws Exception {
 		
 		//given
 		Contest pc = new Contest(2022, 1, "title", "description");
-		// GsonBuilder gsonBuilder = new GsonBuilder();
-        // gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer());
-        // Gson gson = gsonBuilder.setPrettyPrinting().create();
-        // String content = gson.toJson(pc);
 		
 		//when
 		when(dyPc001Service.findAll()).thenReturn(List.of(pc));
@@ -100,17 +101,119 @@ public class DyPc001ControllerTest {
 				)
 		.andDo(print())
 		.andExpect(status().isOk())
-		//.andExpect(content().)
+		//.andExpect(content().string(gson.toJson(data))
 		.andExpect(jsonPath("$[0].yr", is(2022)))
+		.andExpect(jsonPath("$.length()", is(1)))
 		.andReturn().getResponse();
 		;
 		
 		// verify(dyPc001Service).findAll();
 		
+		ResultActions resultActions = mockMvc.perform(
+				post("/v1/api/dypc/list")
+				//.param("key", "value")
+				//.content(content)
+				// .contentType(MediaType.APPLICATION_JSON))
+				// .header("X-AUTH-TOKEN", "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBkb28uY29tIiwicm9sZXMiOlsiUk9MRV9BRE1JTiJdLCJpYXQiOjE2Njg5NTA5NzIsImV4cCI6MTY2ODk1NDU3Mn0.ZOkOQEZE8W2dVTAqcZM_TIhM9DPeNEFl705cvtPZE9o")
+				// .accept(MediaType.APPLICATION_JSON)
+				)
+		//.andDo(print())
+		//.andReturn().getRequest()
+		;
 		//resultActions
 		//.andExpect(status().isOk());
 		
 		//assertThat(response.getStatus()).isEqualTo(HttpStatus.OK);
 		//assertThat(response.getContentLength()).isEqualTo(HttpStatus.OK);
+	}
+	
+	@Test
+	@DisplayName(value = "getOK")
+	@WithMockUser(username = "admin@doo.com", password = "admin", roles = {"ADMIN"})
+	@Order(2)
+	public void getOKTest() throws Exception {
+		
+		// given
+		Contest pc = new Contest(2022, 1, "title", "description");
+		
+		// when
+		when(dyPc001Service.findById(new ContestPK(2022, 1))).thenReturn(Optional.of(pc));
+		
+		// then
+		mockMvc.perform(
+				post("/v1/api/dypc/get")
+				.content("{\"yr\": 2022, \"sn\": 1}")
+				.contentType(MediaType.APPLICATION_JSON)
+		)
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.yr",is(2022)))
+		.andExpect(jsonPath("$.sn", is(1)))
+		.andExpect(jsonPath("$.title", is("title")))
+		.andExpect(jsonPath("$.description", is("description")))
+		;
+	}
+	
+	@Test
+	@DisplayName(value = "getBadRequest")
+	@WithMockUser(username = "admin@doo.com", password = "admin", roles = {"ADMIN"})
+	@Order(3)
+	public void getBadRequestTest() throws Exception {
+		
+		// given
+		Contest pc = new Contest(2022, 1, "title", "description");
+		
+		// when
+		when(dyPc001Service.findById(new ContestPK(2022, 2))).thenReturn(Optional.empty());
+		
+		// then
+		mockMvc.perform(
+				post("/v1/api/dypc/get")
+				.content("{\"yr\": 2022, \"sn\": 2}")
+				.contentType(MediaType.APPLICATION_JSON)
+		)
+		.andExpect(status().isBadRequest())
+		;
+	}
+	
+	@Test
+	@DisplayName(value = "save")
+	@WithMockUser(username = "admin@doo.com", password = "admin", roles = {"ADMIN"})
+	@Order(4)
+	public void saveTest() throws Exception {
+		
+		// given
+		Contest pc = new Contest(2022, 1, "title", "description");
+		
+		// when
+		when(dyPc001Service.save(pc)).thenReturn(pc);
+		
+		// then
+		mockMvc.perform(
+				post("/v1/api/dypc/save")
+				.content("{\"yr\": 2022, \"sn\": 2, \"title\": \"title\", \"description\": \"description\"}")
+				.contentType(MediaType.APPLICATION_JSON)
+		)
+		.andExpect(status().isOk())
+		;
+	}
+	
+	@Test
+	@DisplayName(value = "delete")
+	@WithMockUser(username = "admin@doo.com", password = "admin", roles = {"ADMIN"})
+	@Order(5)
+	public void deleteTest() throws Exception {
+		
+		// given
+		
+		// when
+		
+		// then
+		mockMvc.perform(
+				post("/v1/api/dypc/delete")
+				.content("{\"yr\": 2022, \"sn\": 2}")
+				.contentType(MediaType.APPLICATION_JSON)
+		)
+		.andExpect(status().isOk())
+		;
 	}
 }
