51e8f71899974efda9251ac785fa71a021663a6d
doo <gostbaducking1@nate.com>
2022-12-02 00:54:53 +0900

    feat: dyPb001ControllerTest

--- src/test/java/com/doo/academy/dy/pb/web/DyPb001ControllerTest.java
+++ src/test/java/com/doo/academy/dy/pb/web/DyPb001ControllerTest.java
@@ -2,8 +2,10 @@ package com.doo.academy.dy.pb.web;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
@@ -36,6 +38,7 @@ public class DyPb001ControllerTest {
 	private DyPb001Service dyPb001Service;
 	
 	@Test
+	@DisplayName("listTest")
 	@WithMockUser(username = "admin@doo.com", password = "admin", roles = {"ADMIN"})
 	void listTest() throws Exception {
 
@@ -59,6 +62,67 @@ public class DyPb001ControllerTest {
 		.andExpect(MockMvcResultMatchers.jsonPath("$[1].solution", Matchers.is("solution2")))
 		.andExpect(MockMvcResultMatchers.jsonPath("$[1].answer", Matchers.is("answer2")))
 		.andExpect(MockMvcResultMatchers.jsonPath("$[1].variables", Matchers.is("variables2")))
+		;
+	}
+	
+	@Test
+	@DisplayName("getNotExistTest")
+	@WithMockUser(username = "admin@doo.com", password = "admin", roles = {"ADMIN"})
+	void getNotExistTest() throws Exception {
+
+		Problem problem1 = new Problem(1L, "question1", "solution1", "answer1", "variables1");
+		
+		BDDMockito.given(dyPb001Service.findById(2L)).willReturn(Optional.empty());
+		
+		this.mvc.perform(MockMvcRequestBuilders.post("/v1/api/dypb001/get")
+				.accept(MediaType.APPLICATION_JSON)
+				.content("{\"problemNo\": 2}")
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().isBadRequest())
+		;
+	}
+	
+	@Test
+	@DisplayName("getTest")
+	@WithMockUser(username = "admin@doo.com", password = "admin", roles = {"ADMIN"})
+	void getTest() throws Exception {
+
+		Problem problem1 = new Problem(1L, "question1", "solution1", "answer1", "variables1");
+		
+		BDDMockito.given(dyPb001Service.findById(1L)).willReturn(Optional.of(problem1));
+		
+		this.mvc.perform(MockMvcRequestBuilders.post("/v1/api/dypb001/get")
+				.accept(MediaType.APPLICATION_JSON)
+				.content("{\"problemNo\": 1}")
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$.problemNo", Matchers.is(1)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.question", Matchers.is("question1")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.solution", Matchers.is("solution1")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.answer", Matchers.is("answer1")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.variables", Matchers.is("variables1")))
+		;
+	}
+	
+	@Test
+	@DisplayName("saveTest")
+	@WithMockUser(username = "admin@doo.com", password = "admin", roles = {"ADMIN"})
+	void saveTest() throws Exception {
+
+		Problem problem1 = new Problem(1L, "question1", "solution1", "answer1", "variables1");
+		
+		BDDMockito.given(dyPb001Service.save(problem1)).willReturn(problem1);
+		
+		this.mvc.perform(MockMvcRequestBuilders.post("/v1/api/dypb001/save")
+				.accept(MediaType.APPLICATION_JSON)
+				.content("{\"problemNo\": 1, \"question\": \"question1\", \"solution\": \"solution1\", \"answer\": \"answer1\", \"variables\": \"variables1\"}")
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$.problemNo", Matchers.is(1)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.question", Matchers.is("question1")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.solution", Matchers.is("solution1")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.answer", Matchers.is("answer1")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.variables", Matchers.is("variables1")))
 		;
 	}
 }
