1125ad3c3d5f6719e3b1aa93a7c638498126c701
doo <gostbaducking1@nate.com>
2023-01-01 22:52:54 +0900

    fix: ResponseDto 적용

--- src/main/java/com/doo/academy/dy/pc/web/DyPc001Controller.java
+++ src/main/java/com/doo/academy/dy/pc/web/DyPc001Controller.java
@@ -1,6 +1,5 @@
 package com.doo.academy.dy.pc.web;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.doo.academy.cm.exception.CustomException;
+import com.doo.academy.cm.model.dto.ResponseDto;
 import com.doo.academy.dy.model.pk.ContestPK;
 import com.doo.academy.dy.model.ps.Contest;
 import com.doo.academy.dy.pc.service.DyPc001Service;
@@ -28,32 +28,48 @@ public class DyPc001Controller {
 	private final DyPc001Service dyPc001Service;
 	
 	@RequestMapping(value = "/v1/api/dypc/list", method = RequestMethod.POST)
-	public ResponseEntity<List<Contest>> list() {
+	public ResponseEntity<ResponseDto> list() {
 		
-		return new ResponseEntity<>(dyPc001Service.findAll(), HttpStatus.OK);
+		ResponseDto responseDto = ResponseDto.builder()
+			.code(0)
+			.success(true)
+			.returnObject(dyPc001Service.findAll())
+			.build();
+		return new ResponseEntity<>(responseDto, HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = "/v1/api/dypc/get", method = RequestMethod.POST)
-	public ResponseEntity<Contest> get(@RequestBody Contest contest) throws CustomException {
+	public ResponseEntity<ResponseDto> get(@RequestBody Contest contest) throws CustomException {
 		log.debug("com.doo.academy.dy.pc.web.DyPc001Controller.get.contest : {}", contest);
 		
 		Optional<Contest> oContest = dyPc001Service.findById(new ContestPK(contest.getYr(), contest.getSn()));
 		if (oContest.isEmpty()) {
-			return new ResponseEntity<Contest>(HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<ResponseDto>(HttpStatus.BAD_REQUEST);
 		}
 		log.debug("oContest.get() : {}", oContest.get());
-		return new ResponseEntity<>(oContest.get(), HttpStatus.OK);
+		
+		ResponseDto responseDto = ResponseDto.builder()
+			.code(0)
+			.success(true)
+			.returnObject(oContest.get())
+			.build();
+		return new ResponseEntity<>(responseDto, HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = "/v1/api/dypc/save", method = RequestMethod.POST)
-	public ResponseEntity<Contest> save(@RequestBody Contest contest) throws CustomException {
+	public ResponseEntity<ResponseDto> save(@RequestBody Contest contest) throws CustomException {
 		log.debug("com.doo.academy.dy.pc.web.DyPc001Controller.save.contest : {}", contest);
 		
-		return new ResponseEntity<>(dyPc001Service.save(contest), HttpStatus.OK);
+		ResponseDto responseDto = ResponseDto.builder()
+			.code(0)
+			.success(true)
+			.returnObject(dyPc001Service.save(contest))
+			.build();
+		return new ResponseEntity<>(responseDto, HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = "/v1/api/dypc/delete", method = RequestMethod.POST)
-	public ResponseEntity<Contest> delete(@RequestBody Contest contest) throws CustomException {
+	public ResponseEntity<ResponseDto> delete(@RequestBody Contest contest) throws CustomException {
 		log.debug("com.doo.academy.dy.pc.web.DyPc001Controller.delete.contest : {}", contest);
 		
 		dyPc001Service.delete(new ContestPK(contest.getYr(), contest.getSn()));
